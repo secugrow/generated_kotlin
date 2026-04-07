78e518945ac48ad9566de31193cf2efac4a37c63
boris779 <boris.wrubel@gmx.at>
2026-04-07 10:50:30 +0000

    Update from https://github.com/secugrow/kotlin-archetype/commit/9ed2fac60a48ee26b901bad044ee2e92dbaa3cde

--- src/test/kotlin/at/some/test/pageobjects/WikipediaStartPage.kt
+++ src/test/kotlin/at/some/test/pageobjects/WikipediaStartPage.kt
@@ -2,7 +2,6 @@ package at.some.test.pageobjects;
 
 import at.some.test.driverutil.WebDriverSession
 import org.openqa.selenium.By
-import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.ui.ExpectedConditions
 
 class WikipediaStartPage(session: WebDriverSession) : MainPage(session) {
@@ -16,13 +15,12 @@ class WikipediaStartPage(session: WebDriverSession) : MainPage(session) {
         return WikipediaContentPage(session)
     }
 
-    fun searchFor(searchstring: String?): WikipediaContentPage {
-        getSearchbar().sendKeys(searchstring)
-        submitSearch()
-        return WikipediaContentPage(session)
-    }
-
-    fun getSearchbar(): WebElement {
-        return wdwait.until(ExpectedConditions.presenceOfElementLocated(By.id("searchInput")))
+    fun searchFor(searchstring: String): WikipediaContentPage {
+        wdwait.until { driver ->
+            requireNotNull(
+                ExpectedConditions.presenceOfElementLocated(By.id("searchInput")).apply(driver)
+            ) { "Searchbar not found" }
+        }.sendKeys(searchstring)
+        return submitSearch()
     }
 }
